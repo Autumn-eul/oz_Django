d00f70d879c59b7d38ad235750193e54ef5f859b
Autumn-eul <dlrkdmf1052@naver.com>
2024-12-10 22:37:39 +0900

    📦️ Django chapter3
    django chapter3

--- oz_django/boards/models.py
+++ oz_django/boards/models.py
@@ -10,5 +10,7 @@ class Board(CommonModel):
     likes = models.PositiveIntegerField(default = 0)
     reviews = models.PositiveIntegerField(default = 0)
 
+    user = models.ForeignKey("users.User", on_delete = models.CASCADE) # 유저가 삭제되면 게시글도 삭제하겠다
+
     def __str__(self):
         return self.title
